96a58a375110ef731b87e7328dc8f2933cab89e0
Gaurav-n26 <gaurav.ratnawat@n26.com>
2025-05-22 17:39:26 +0200

    Task-1 | test: should initialize cells with given value

---
+++ src/test/kotlin/com/gridgame/CellTest.kt
@@ -0,0 +1,25 @@
+package com.gridgame
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CellTest {
+
+    private lateinit var grid: Grid
+    private lateinit var cell: Cell
+
+    @BeforeEach
+    fun setUp() {
+        grid = Grid()
+        cell = Cell(row = 1, column = 2, value = 0, grid = grid)
+    }
+
+    @Test
+    fun `should initialize the cells `() {
+        assertEquals(1, cell.row)
+        assertEquals(2, cell.column)
+        assertEquals(0, cell.value)
+        assertSame(grid, cell.grid)
+    }
+}
