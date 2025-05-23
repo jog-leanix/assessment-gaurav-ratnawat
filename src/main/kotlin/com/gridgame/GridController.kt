91a75a8b325ae6b86b9f03cc796519ebc4cf55e4
Gaurav-n26 <gaurav.ratnawat@n26.com>
2025-05-23 12:45:03 +0200

    Task-1 | impl: should create grid and return 201

---
+++ src/main/kotlin/com/gridgame/GridController.kt
@@ -0,0 +1,28 @@
+package com.gridgame
+
+import com.gridgame.service.GridService
+import jakarta.inject.Inject
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.QueryParam
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+
+@Path("/grid")
+@Produces(MediaType.APPLICATION_JSON)
+class GridController {
+
+    @Inject
+    lateinit var gridService: GridService
+
+    @POST
+    fun createGrid(
+        @QueryParam("rows") rows: Int = 50,
+        @QueryParam("columns") columns: Int = 50
+    ): Response {
+        val createdGrid = gridService.initializeGrid(rows, columns)
+        return Response.status(Response.Status.CREATED).entity(createdGrid).build()
+    }
+}
